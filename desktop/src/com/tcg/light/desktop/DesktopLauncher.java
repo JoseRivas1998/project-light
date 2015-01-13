b90042ba629da004be2c6e8aa4ac3c9b3f54de5e
Jose Rivas <JoseRivas823@gmail.com>
2015-01-12 19:51:03 -0800

    Initial Commit to Repo

---
+++ desktop/src/com/tcg/light/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcg.light.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcg.light.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
